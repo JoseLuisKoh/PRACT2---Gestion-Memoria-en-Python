10a48fccf88e95b2b9c51e1e84de6a96958668a3
JoseLuisKoh <149828327+JoseLuisKoh@users.noreply.github.com>
2024-02-09 15:21:29 -0600

    Add files via upload

---
+++ calificaciones.py
@@ -0,0 +1,9 @@
+
+
+
+calificaciones = [0] * 5
+
+for i in range(5):
+    calificaciones[i] = int(input("Captura la calificación:"))
+
+print(calificaciones)
---
+++ frutas.py
@@ -0,0 +1,11 @@
+
+
+frutas = ["mango", "Manzana", "Banana", "uvas"]
+
+print(frutas)
+
+frutas.pop(0)
+frutas.pop(1)
+frutas.insert(1, "sandia")
+
+print(frutas)
